876eb5af69492eb17fc0b6c05e71ef9f04d798db
A-lineed <aline.edvania.franca@outlook.com>
2025-04-08 16:22:16 -0300

    Checking and unchecking checkboxes

--- cypress/e2e/playground.cy.js
+++ cypress/e2e/playground.cy.js
@@ -7,10 +7,32 @@ describe('template spec', () => {
     cy.get('#promotional-banner').should('be.visible')
   })
 
-  it.only('Clicks the subscribe button and shows a successs message', () => {
+  it('Clicks the subscribe button and shows a successs message', () => {
     cy.contains('button', 'Subscribe').click()
 
     cy.contains('#success', "You've been successfully subscribed to our newsletter.")
       .should('be.visible')
   })
+
+  it('Types in an input which "signs" a form, then asserts it is signed', () => {
+    cy.get('#signature-textarea').type('Aline Edvania')
+
+    cy.contains('#signature', 'Aline Edvania').should('be.visible')
+  })
+
+  it.only('Types in the signature field, checks the checkbox to see the preview, then unchecks it', () => {
+    cy.get('#signature-textarea-with-checkbox').type('Aline Edvania')
+
+    cy.get('#signature-checkbox').check()
+      .should('be.checked')
+
+    cy.contains('#signature-triggered-by-check', 'Aline Edvania').should('be.visible')
+
+    cy.get('#signature-checkbox').uncheck()
+      .should('not.be.checked')
+    
+      cy.contains('#signature-triggered-by-check', 'Aline Edvania').should('not.exist')
+  })
+
+
 })
